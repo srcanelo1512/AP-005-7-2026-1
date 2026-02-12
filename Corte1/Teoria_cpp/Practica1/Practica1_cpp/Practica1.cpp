3bf2042d077a6bfe9a28846d55234e520a959798
srcanelo1512 <collazos.erick1512@gmail.com>
2026-02-12 14:21:19 -0500

    Practica1.cpp

---
+++ Corte1/Teoria_cpp/Practica1/Practica1_cpp/Practica1.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Biblioteca necesaria para entrada/salida
+
+int main() {
+    // std::cout se usa para imprimir texto en la consola
+    std::cout << "Hola Mundo" << std::endl; 
+    return 0; // Indica que el programa finalizó correctamente
+}
